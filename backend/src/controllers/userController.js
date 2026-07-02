d09a71037b23896e91397d2aa013091f21815097
Truong Luu <dinhluu1259@gmail.com>
2026-07-02 10:09:58 +0700

    feat(auth): add user login and logout

---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,171 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import validator from "validator";
+import userModel from "../models/User.js";
+import crypto from "crypto";
+import Session from "../models/Session.js";
+
+const ACCESS_TOKEN_TTL = "30m";
+const REFRESH_TOKEN_TTL = 7 * 24 * 60 * 60 * 1000;
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    // checking for all data to register user
+    if (!name || !email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Missing Details" });
+    }
+
+    // validating email format
+    if (!validator.isEmail(email)) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Please enter a valid email" });
+    }
+
+    // checking if user already exists
+    const existingUser = await userModel.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(409)
+        .json({ success: false, message: "User already exists" });
+    }
+
+    // validating strong password
+    if (password.length < 8) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Please enter a strong password" });
+    }
+
+    // hashing user password
+    const salt = await bcrypt.genSalt(10); // the more no. round the more time it will take
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const userData = {
+      name,
+      email,
+      password: hashedPassword,
+    };
+
+    const newUser = new userModel(userData);
+    const user = await newUser.save();
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: ACCESS_TOKEN_TTL,
+    });
+
+    res.status(201).json({
+      success: true,
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error("Error while signing up user:", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Kiểm tra dữ liệu đầu vào
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Email and password are required",
+      });
+    }
+
+    // Chuẩn hóa email
+    const normalizedEmail = email.trim().toLowerCase();
+
+    // Tìm user
+    const user = await userModel.findOne({ email: normalizedEmail });
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    // So sánh mật khẩu
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    // Tạo JWT
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: ACCESS_TOKEN_TTL,
+    });
+
+    // tạo refresh token
+    const refreshToken = crypto.randomBytes(64).toString("hex");
+
+    //lưu refresh token vào database
+    await Session.create({
+      userId: user._id,
+      refreshToken,
+      expiresAt: new Date(Date.now() + REFRESH_TOKEN_TTL), // 7 ngày
+    });
+    // Trả refreshtoken về trong cookie
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      maxAge: REFRESH_TOKEN_TTL,
+    });
+
+    // Trả kết quả
+    return res.status(200).json({
+      success: true,
+      message: "Login successfully",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        imageUrl: user.imageUrl,
+      },
+    });
+  } catch (error) {
+    console.error("Login Error:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+
+export const logoutUser = async (req, res) => {
+  try {
+    // lấy refresh token từ cookie
+    const refreshToken = req.cookies.refreshToken;
+
+    if (refreshToken) {
+      // xóa refresh token trong db
+      await Session.deleteOne({ refreshToken });
+
+      // xóa refresh token trong cookie
+      res.clearCookie("refreshToken");
+    }
+
+    return res.status(204);
+  } catch (error) {
+    console.error("Logout Error:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import {connectDB} from "./libs/db.js";
-import authRoute from "./routes/authRoute.js";
+import authRoute from "./routes/userRoute.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 //middleware
 app.use(express.json());
+app.use(cookieParser());
 
 //public routes
 app.use('/api/auth', authRoute);
